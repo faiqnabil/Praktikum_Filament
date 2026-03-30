5ec7ae504c0799b2d29cc0010bc31615422abd42
Faiq Nabil <fq28nabil@gmail.com>
2026-03-30 18:41:14 +0700

    Filament JS-13

--- app/Filament/Resources/Posts/Tables/PostsTable.php
+++ app/Filament/Resources/Posts/Tables/PostsTable.php
@@ -7,12 +7,16 @@ use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Columns\ColorColumn;
 use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\IconColumn;
+use Filament\Actions\ReplicateAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\DeleteAction;
 use Filament\Tables\Filters\Filter;
 use Filament\Forms\Components\DatePicker;
 use Filament\Tables\Filters\SelectFilter;
+use Filament\Actions\Action;
+use Filament\Forms\Components\Checkbox;
 
 class PostsTable
 {
@@ -92,7 +96,19 @@ class PostsTable
             ])
                 
             ->recordActions([
+                ReplicateAction::make(),
                 EditAction::make(),
+                DeleteAction::make(),
+                Action::make("status")
+                    ->Label("Status Change")
+                    ->icon('heroicon-o-check-circle')
+                    ->schema([
+                        Checkbox::make('published')
+                            ->default(fn($record): bool => $record->published),
+                    ])
+                    ->action(function ($record, $data) {
+                        $record->update(['published' => $data['published']]);
+                    })
             ])
 
             ->toolbarActions([
